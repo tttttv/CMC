3bfb02071cccae29825ff664c8bfdb0651b5c4e9
Vladimir Kurtynov <Vlapvlapvlap1>
2024-03-25 22:04:25 +0300

    redis env

--- web/CMC/celery.py
+++ web/CMC/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CMC.settings')
-app = Celery('CMC', broker='redis://' + os.getenv('REDIS_USER') + ':' + os.getenv('REDIS_PASSWORD') + '@redis:6379/0')
+app = Celery('CMC', broker='redis://' + os.getenv('REDIS_USER', '') + ':' + os.getenv('REDIS_PASSWORD', '') + '@redis:6379/0')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
