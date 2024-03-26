0c72c387f33a8cf95c8e19bb477b9d09a5c17368
Vladimir Kurtynov <Vlapvlapvlap1>
2024-03-26 03:01:55 +0300

    initial settings

--- web/CMC/settings.py
+++ web/CMC/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': 5432,
+        'PORT': '5432',
     },
 }
 
